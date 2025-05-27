8036d2a47f4426cf7532cf44fa1bef1a0c1b47bf
ppieniazek <patryk.pieniazek2001@gmail.com>
2025-05-27 23:04:57 +0200

    CH2: Cells L3: Draw Move

--- graphics.py
+++ graphics.py
@@ -1,18 +1,38 @@
 from tkinter import BOTH, Canvas, Tk
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
+        )
+
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=True)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
+
+    def draw_line(self, line: Line, fill_color: str = "black"):
+        line.draw(self.__canvas, fill_color)
 
     def wait_for_close(self):
         self.__running = True
@@ -21,3 +41,76 @@ class Window:
 
     def close(self):
         self.__running = False
+
+
+class Cell:
+    def __init__(self, win: Window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if self.__win is None:
+            return
+
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+        if self.has_left_wall:
+            wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(wall)
+
+        if self.has_right_wall:
+            wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(wall)
+
+        if self.has_top_wall:
+            wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(wall)
+
+        if self.has_bottom_wall:
+            wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(wall)
+
+    def draw_move(self, to_cell, undo=False):
+        start = Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
+        end = Point(
+            (to_cell.__x1 + to_cell.__x2) / 2, (to_cell.__y1 + to_cell.__y2) / 2
+        )
+        color = "gray" if undo else "red"
+        self.__win.draw_line(Line(start, end), color)
+
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+
+        self.__create_cells()
+
+    def __create_cells(self):
+        self.__cells = []
+        #! WIP
