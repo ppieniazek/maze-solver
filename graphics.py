8ebf240d5cd30541365e3b802ef544ff7934b777
ppieniazek <patryk.pieniazek2001@gmail.com>
2025-05-28 22:18:50 +0200

    CH3: Maze L1: Maze Class

--- graphics.py
+++ graphics.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import BOTH, Canvas, Tk
 
 
@@ -98,7 +99,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win: Window,
     ):
         self.__x1 = x1
         self.__y1 = y1
@@ -112,5 +113,27 @@ class Maze:
         self.__create_cells()
 
     def __create_cells(self):
-        self.__cells = []
-        #! WIP
+        self.__cells = [
+            [Cell(self.__win) for _ in range(self.__num_rows)]
+            for _ in range(self.__num_cols)
+        ]
+
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i, j):
+        if self.__win is None:
+            return
+        x1 = self.__x1 + i * self.__cell_size_x
+        y1 = self.__y1 + j * self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self.__win is None:
+            return
+        self.__win.redraw()
+        time.sleep(0.03)
